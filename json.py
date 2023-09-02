05e7be2feba4f49fa152ad43e69fa8b1e21a53f2
pedrocpacheco <cpedropacheco@hotmail.com>
2023-09-02 16:24:53 -0300

    feat: find Analyst by Rm

--- json.py
+++ json.py
@@ -7,6 +7,23 @@ def customerByCpf(cpf):
       if customer["cpf"] == cpf:
         return customer
   print("Customer not found")
+
+def analystByRm(rm):
+  with open ("./analyst.json") as file: 
+    data = json.load(file)
+    for analyst in data:
+      if analyst["rm"] == rm:
+        return analyst
+  print("Analyst not Found")
+  
+def findInJson(credential, file):
+  data = json.loaad(file)
+  for person in data:
+    if person["rm"] == credential or person["cpf"] == credential:
+      return person
       
 print("Retornando Customer:")   
 print(customerByCpf("51670111162"))
+
+print("Retornando Analyst: ")
+print(analystByRm("RM9158167013"))
